418eec9a6fd68ba9ccf8db5f66c756865a020d8a
Gregory Skiano <gregoryskiano@gmail.com>
2017-10-31 20:45:51 -0700

    adding readme

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 
-// not as elegant as using ...
-// but no build!
-function compose () {
-  const fns = Array.from(arguments)
-  return (input) => fns.reduceRight((output, fn) => fn(output), input)
-}
+// given a template looking something like this:
+
+//   ABA
+//   BAB
+
+// where letters represent valid arrangements of 'blocks',
+// this will create an engine that will layout a field of blocks
+// using only the connections available in the template
+
+// For example, the template above could not produce the following:
+
+//   AAA
+//   AAA
+
+// Because there is no example of an A ajoining an A in the template
+
 
 function pipe () {
   const fns = Array.from(arguments)
@@ -21,10 +31,12 @@ const neighbors = [
 const add2D = (a, b) => ([a[0] + b[0], a[1] + b[1]])
 
 const eachNeighbor = (point, fn) => {
-  neighbors.forEach(([id, n], idx) => fn(add2D(point, n), id))
+  neighbors
+    .forEach(([direction, delta], idx) => 
+      fn(add2D(point, delta), direction))
 }
 
-const templateToGrid = (temlate) => (
+const templateToGrid = (template) => (
   template
     .split('\n')
     .map(s => s.trim())
@@ -36,12 +48,21 @@ const gridToNeighbors = (grid) => {
   const map = {}
 
   grid.forEach((row, y) => {
-    row.forEach((col, x) => {
-      eachNeighbor([x, y], ([nX, nY], id) => {
-        console.log(id, grid[nY] && grid[nY][nX])
+    row.forEach((current, x) => {
+      eachNeighbor([x, y], ([nX, nY], direction) => {
+        const neighbor = (grid[nY] && grid[nY][nX]) || '-'
+
+        if (!map[current]) map[current] = {}
+        if (!map[current][direction]) map[current][direction] = []
+        
+        if (!map[current][direction].includes(neighbor)) {
+          map[current][direction].push(neighbor)  
+        }
       })
     })
   })
+
+  return map
 }
 
 const processTemplate = pipe(
@@ -49,12 +70,15 @@ const processTemplate = pipe(
   gridToNeighbors,
 )
 
-
-const template = `
+console.log(
+  processTemplate(`
   ABA
   BAB
-`
+`)
+)
 
-processTemplate(template)
+console.log(
+  processTemplate('BAB')
+)
 
 
